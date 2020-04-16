e164890ca54ecd2abfdc85ec9e6ed9956dff9202
koodaa-team <kari@koodaa.com>
2020-04-16 23:36:59 +0700

    First Commit

---
+++ ex1-linear-regression/ex1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+print("=============================================")
+print("Coursera Machine Learning - Python Version")
+print("EXERCISE 1 - Linear Regression")
+print("=============================================")
+
+print("Warming Excercise")
+# Show an identity matrix 5x5
+identity_matrix = np.identity(5)
+print("Identity matrix 5x5 is \n {}".format(identity_matrix))
+
